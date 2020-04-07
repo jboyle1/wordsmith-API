352aed3b9e243c07e709182425781d5b0f0786d4
Jed Boyle <jboyle1@pca.ac.uk>
2020-04-07 10:22:24 +0100

    Added 'AJAX function' GET request to main.js

--- public/js/main.js
+++ public/js/main.js
@@ -7,4 +7,20 @@ const inputField = document.querySelector("#input");
 const submit = document.querySelector("#submit");
 const responseField = document.querySelector("#responseField");
 
+// AJAX function
+const getSuggestions = () => {
+    const wordQuery = inputField.value;
+    const endpoint = `${url}${queryParams}${wordQuery}`;
 
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            renderResponse(xhr.response);
+        }
+    }
+
+    xhr.open("GET", endpoint);
+    xhr.send();
+}
