4bea3576e5066ea7766cd940c2afb3f2f577f1bd
Jed Boyle <jboyle1@pca.ac.uk>
2020-04-07 10:12:51 +0100

    Added 'Selecting page elements' to main.js.

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,9 @@
 const url = "https://api.datamuse.com/words?";
 const queryParams = "rel_rhy=";
 
+// Selecting page elements
+const inputField = document.querySelector("#input");
+const submit = document.querySelector("#submit");
+const responseField = document.querySelector("#responseField");
+
+
