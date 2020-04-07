525ed63a0f7158458bba03f64920f319f6175c99
Jed Boyle <jboyle1@pca.ac.uk>
2020-04-07 10:29:13 +0100

    Added 'Clear previous results and display results to webpage' with event listener (onClick), to main.js.

--- public/js/main.js
+++ public/js/main.js
@@ -24,3 +24,14 @@ const getSuggestions = () => {
     xhr.open("GET", endpoint);
     xhr.send();
 }
+
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+    event.preventDefault();
+    while(responseField.firstChild){
+        responseField.removeChild(responseField.firstChild);
+    }
+    getSuggestions();
+}
+
+submit.addEventListener("click", displaySuggestions);
